808d2a14f07f3de9c466bff072e0b5a812f4a5c8
Thomas Voss <mail@thomasvoss.com>
2024-01-01 18:57:20 +0100

    Switch rune_t to a char32_t from uint32_t

--- src/uni.h
+++ src/uni.h
@@ -1,13 +1,13 @@
 #ifndef ANDY_UNI_H
 #define ANDY_UNI_H
 
-#include <stdint.h>
+#include <uchar.h>
 
 /* U+FFFD REPLACEMENT CHARACTER; intended for use when invalid UTF-8 sequences
    are detected. */
 #define UNI_REPL_CHAR ((rune_t)0xFFFD)
 
-typedef uint32_t rune_t;
+typedef char32_t rune_t;
 
 /* Assert whether the given rune is a unicode whitespace character. */
 [[gnu::const]] bool unispace(rune_t);
