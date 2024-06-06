042e5f80e34796a2ff4108cf1a51ff459447b24f
Thomas Voss <mail@thomasvoss.com>
2024-06-06 14:37:18 +0200

    Support negative numbers

--- src/bigint.c
+++ src/bigint.c
@@ -5,6 +5,11 @@ isbigint(struct u8view sv)
 {
 	if (sv.len == 0)
 		return false;
+	if (sv.p[0] == '-') {
+		if (sv.len == 1)
+			return false;
+		VSHFT(&sv, 1);
+	}
 	if (sv.p[0] == '0' && sv.len > 1)
 		return false;
 	for (size_t i = 0; i < sv.len; i++) {
@@ -19,11 +24,25 @@ bigintcmp(const void *_x, const void *_y)
 {
 	struct u8view *x = (struct u8view *)_x;
 	struct u8view *y = (struct u8view *)_y;
+
+	int ret;
+	bool neg = false;
+
+	if (x->p[0] == '-') {
+		if (y->p[0] != '-')
+			return -1;
+		neg = true;
+	} else if (y->p[0] == '-')
+		return +1;
+
 	if (x->len != y->len)
-		return x->len > y->len ? +1 : -1;
-	for (size_t i = 0; i < x->len; i++) {
-		if (x->p[i] != y->p[i])
-			return x->p[i] > y->p[i] ? +1 : -1;
+		ret = x->len > y->len ? +1 : -1;
+	else for (size_t i = 0; i < x->len; i++) {
+		if (x->p[i] != y->p[i]) {
+			ret = x->p[i] > y->p[i] ? +1 : -1;
+			break;
+		}
 	}
-	return 0;
+
+	return neg ? -ret : ret;
 }
